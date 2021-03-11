f117f4cbf964f3541caa25b53cd3ef636c712cc5
Beyza <76918032+BeyzaKayraklik@users.noreply.github.com>
2021-03-11 23:42:52 +0300

    Add files via upload

---
+++ Homeworks/Homework2.py
@@ -0,0 +1,20 @@
+beyzaKayraklik={"okuduğu üniversite":"selçuk üniversitesi","sınıf:":2,"not ortalaması:":3.12,"yaş:":21}
+sedaÖzdemir={"okuduğu üniversite":"İTÜ","sınıf:":3,"not ortalaması:":2.37 ,"yaş:":23}
+enesMeydan={"okuduğu üniversite":"ODTÜ","sınıf:":1,"not ortalaması:":3.00 ,"yaş:":27}
+esraÖrs={"okuduğu üniversite":"BAU","sınıf:":1,"not ortalaması:":2.10 ,"yaş:":20}
+esmaYesiltepe={"okuduğu üniversite":"Konya Teknik","sınıf:":2,"not ortalaması:":3.40 ,"yaş:":21}
+
+for i in beyzaKayraklik.items():
+    print("Beyza Kayraklik : ", i )
+
+for i in sedaÖzdemir.items():
+    print("Seda Özdemir :" , i)
+
+for i in enesMeydan.items():
+    print("Enes Meydan :" , i)
+
+for i in esraÖrs.items():
+    print("Esra Örs :", i)
+
+for i in esmaYesiltepe.items():
+    print("Esma Yesiltepe:" ,i)
